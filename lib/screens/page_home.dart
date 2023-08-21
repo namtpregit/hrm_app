981e62e186bae64652e464086e3c7d07d0dca355
phuongnam250196 <namtp@regit-technology.com>
2023-08-21 22:39:24 +0700

    Update screen

--- lib/screens/page_home.dart
+++ lib/screens/page_home.dart
@@ -36,6 +36,7 @@ class HomePage extends StatelessWidget {
             children: [
               Header(),
               Box1(),
+              BoxTask(),
               Text('Home'),
             ],
           ),
@@ -71,28 +72,96 @@ class Box1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-        child: Container(
-      color: const Color(0xffE9F0F2),
-      width: double.infinity,
-      margin: const EdgeInsets.only(bottom: 2.0),
-      child: Padding(
-        padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              '26-8-2023 (Thứ 4)',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Row(
-              children: [
-                ElevatedButton(onPressed: () {}, child: const Text('Checkin')),
-                ElevatedButton(onPressed: () {}, child: const Text('Checkin')),
-              ],
-            )
-          ],
+      child: Container(
+        color: const Color(0xffE9F0F2),
+        width: double.infinity,
+        margin: const EdgeInsets.only(bottom: 16.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '26-8-2023 (Thứ 4)',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffC7471E),
+                    textStyle: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  child: const Text('Checkin'),
+                ),
+                const VerticalDivider(),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffA9A5A3),
+                    textStyle: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  child: const Text('Checkout'),
+                ),
+              ])
+            ],
+          ),
         ),
       ),
-    ));
+    );
+  }
+}
+
+class BoxTask extends StatelessWidget {
+  const BoxTask({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      elevation: 10,
+      color: Colors.grey,
+      shadowColor: Colors.grey,
+      borderRadius: BorderRadius.circular(2),
+      child: Container(
+        color: const Color(0xffE9F0F2),
+        width: double.infinity,
+        margin: const EdgeInsets.only(bottom: 2.0),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                '26-8-2023 (Thứ 4)',
+                style: TextStyle(fontWeight: FontWeight.bold),
+              ),
+              Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffC7471E),
+                    textStyle: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  child: const Text('Checkin'),
+                ),
+                const VerticalDivider(),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xffA9A5A3),
+                    textStyle: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.bold),
+                  ),
+                  child: const Text('Checkout'),
+                ),
+              ])
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
